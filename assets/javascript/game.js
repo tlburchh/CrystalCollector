e3f70cbc39d86d00a09492ee1b206ff70f41b458
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-06-19 07:57:14 -0400

    initial javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var targetNumber
+var score = 0
+var winCount = 0
+var lossCount = 0
+var crystalCount
+
+// make target number range between 40 and 60
+targetNumber = math.floor(math.random() *56) +5;
+// create random numbers for crystals rangine between 5 and 15
+// create connection between numbers, crystal count and crystal ID
+
+    // make images buttons and connect to previous
+
+    // have clicks adjust score
+
